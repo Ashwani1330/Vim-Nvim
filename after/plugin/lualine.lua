9cebe85afc8e620482b411e50b406701d874fa89
Ashwani1330 <ashwanihabri@gmail.com>
2024-07-10 21:32:57 +0530

    lualine customisation

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,13 @@
-require('lualine').setup({
-    options = {--theme = 'gruvbox,
-    section_separators = '', component_separators = ''}
-})
+require('lualine').setup {
+    options = {
+        component_separators = '',
+        section_separators = { left = '', right = '' },
+    },
+    sections = {
+        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+        lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+        }
+    }
+}
 
